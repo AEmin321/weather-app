8d79a4ff31279db09320841ff5fbba549e4bdebf
AEmin321 <ahmademin321@gmail.com>
2023-03-31 01:03:59 +0300

    Added the function to display 7 day dat on cards

--- src/modules/data.js
+++ src/modules/data.js
@@ -1,13 +1,13 @@
 async function getData (city) {
     try {
-        const endKey=`http://api.weatherapi.com/v1/current.json?key=523aad0fd36442f98a4141313232903&q=${city}`;
-        const fetchForeCastData=await fetch(endKey,{mode:'cors'});
-        if (!fetchForeCastData.ok){
+        const endKey=`http://api.weatherapi.com/v1/forecast.json?key=523aad0fd36442f98a4141313232903&q=${city}&days=7`;
+        const response=await fetch(endKey,{mode:'cors'});
+        if (!response.ok){
             throw new Error(`Sorry cant find ${city}.`);
         }
-        const foreCastData=await fetchForeCastData.json();
-        console.log (foreCastData);
-        return foreCastData;
+        const data=await response.json();
+        console.log (data);
+        return data;
     }
     catch(error) {
         alert(error);
--- src/modules/interface.js
+++ src/modules/interface.js
@@ -2,27 +2,42 @@ import getData from "./data";
 
 const domController=(()=>{
     const currentCard=document.querySelector('.current-card');
-    // console.log(data);
-    async function defaultCurrentWeather (){
-        const data=await getData('Konya');
-        currentCard.innerHTML=`<div class="temp">${data.current.temp_c}&deg;</div>
+    const daysWrapper=document.querySelector('.days-wrapper');
+
+    async function handleCurrentWeather (city){
+        const data1=await getData('Konya');
+        currentCard.innerHTML=`<div class="temp">${data1.current.temp_c}&deg;</div>
         <div class="weather-headings">
             <div class="weather-img">
-                <img src="${data.current.condition.icon}" alt="">
+                <img src="${data1.current.condition.icon}" alt="">
             </div>
             <div class="weather-details">
-                <div class="weather">${data.current.condition.text}</div>
-                <div class="location">${data.location.name}, ${data.location.country}</div>
+                <div class="weather">${data1.current.condition.text}</div>
+                <div class="location">${data1.location.name}, ${data1.location.country}</div>
             </div>
         </div>
-        <div class="last-updated">${data.current.last_updated}</div>
+        <div class="last-updated">${data1.current.last_updated}</div>
         <div class="current-details">
-            <p><i class="fa-solid fa-temperature-three-quarters fa-lg" style="color: #232931;"></i>${data.current.feelslike_c}</p>
-            <p><i class="fa-solid fa-wind fa-lg" style="color: #232931;"></i>${data.current.wind_mph}</p>
-            <p><i class="fa-solid fa-water fa-lg" style="color: #232931;"></i>${data.current.humidity}</p>
+            <p><i class="fa-solid fa-temperature-three-quarters fa-lg" style="color: #232931;"></i>${data1.current.feelslike_c}</p>
+            <p><i class="fa-solid fa-wind fa-lg" style="color: #232931;"></i>${data1.current.wind_mph}</p>
+            <p><i class="fa-solid fa-water fa-lg" style="color: #232931;"></i>${data1.current.humidity}</p>
         </div>`;
     }
-    defaultCurrentWeather();
+
+    async function handleFutureWeather(city) {
+        const data=await getData('konya');
+        data.forecast.forecastday.forEach(day => {
+            const Card=document.createElement('div');
+            Card.classList.add('day-card');
+            Card.innerHTML=`<div class="weater-img"><img src="${day.day.condition.icon}" alt="weather image"></div>
+            <div class="weather-temp">${day.day.avgtemp_c}&deg;</div>
+            <div class="weather-title">${day.day.condition.text}</div>
+            <div class="weather-day">${day.date}</div>`;
+            daysWrapper.appendChild(Card);
+        });
+    }
+    handleCurrentWeather();
+    handleFutureWeather('asdf');
 })()
 
 export default domController;
