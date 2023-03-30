fa79438d5ed6f26a17cc5a88c7e2fae592103d54
AEmin321 <ahmademin321@gmail.com>
2023-03-30 17:34:38 +0300

    Added the current weather layout and style

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # weather-app
+
+
+
+
+Image by <a href="https://pixabay.com/users/julius_silver-4371822/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3021072">Julius Silver</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3021072">Pixabay</a>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 async function getData () {
     try {
-        const fetchData=await fetch('http://api.weatherapi.com/v1/current.json?key=523aad0fd36442f98a4141313232903&q=paris',{mode:'cors'}); 
-        const dataSet=await fetchData.json();
-        console.log (dataSet);
+        // const fetchCurrentData=await fetch('http://api.weatherapi.com/v1/current.json?key=523aad0fd36442f98a4141313232903&q=paris',{mode:'cors'}); 
+        // const currentData=await fetchCurrentData.json();
+        const fetchForeCastData=await fetch('http://api.weatherapi.com/v1/current.json?key=523aad0fd36442f98a4141313232903&q=paris',{mode:'cors'});
+        const foreCastData=await fetchForeCastData.json();
+        // console.log (currentData);
+        console.log (foreCastData);
     }
     catch(error) {
         console.log (error);
