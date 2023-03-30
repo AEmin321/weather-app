a2567f3bc8ac82c8c25346add4a8835639854cb5
AEmin321 <ahmademin321@gmail.com>
2023-03-30 03:49:02 +0300

    Added the function to fetch data to console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log ('webpack heeeyy');
+async function getData () {
+    try {
+        const fetchData=await fetch('http://api.weatherapi.com/v1/current.json?key=523aad0fd36442f98a4141313232903&q=paris',{mode:'cors'}); 
+        const dataSet=await fetchData.json();
+        console.log (dataSet);
+    }
+    catch(error) {
+        console.log (error);
+    }
+}
+
+getData();
